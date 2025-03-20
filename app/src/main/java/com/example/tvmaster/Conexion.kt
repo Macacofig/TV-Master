5e32415941ab870d21a7e4c83c09f492b70b306d
lucasalcoba <lucasalcoba74@gmail.com>
2025-03-20 14:34:10 -0400

    conectado y no conectado

---
+++ app/src/main/java/com/example/tvmaster/Conexion.kt
@@ -0,0 +1,135 @@
+package com.example.tvmaster
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+
+
+@Composable
+fun ConexionScreen() {
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(Color.Black), // Fondo negro
+        contentAlignment = Alignment.Center
+    ) {
+        Row (
+            modifier = Modifier
+                .align(Alignment.TopStart)
+                .padding(16.dp)
+        ){
+            IconButton(
+                onClick = { /* Acción de volver */ },
+                modifier = Modifier
+                    .padding(1.dp)
+            ) {
+                Icon(
+                    imageVector = Icons.Default.ArrowBack,
+                    contentDescription = "Volver",
+                    tint = Color.White
+                )
+            }
+            Text(
+                text = stringResource( id = R.string.conection),
+                modifier = Modifier.padding(10.dp),
+                color = Color.White,
+                fontSize = 24.sp,
+                fontWeight = FontWeight.Bold
+
+            )
+        }
+        Column(
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Center
+        ) {
+            // Texto de conexión
+
+
+            Spacer(modifier = Modifier.height(30.dp))
+
+            // Icono de TV
+            Image(
+                painter = painterResource(id = R.drawable.tv),
+                contentDescription = "TV",
+                modifier = Modifier.size(100.dp) // Tamaño del icono
+            )
+
+            Spacer(modifier = Modifier.height(20.dp))
+
+            // Iconos de los puntitos de conexión
+            Image(
+                painter = painterResource(id = R.drawable.puntitos_pantalla_conexion),
+                contentDescription = "Puntitos de conexión",
+                modifier = Modifier.size(50.dp) // Tamaño de los puntitos
+            )
+            Image(
+                painter = painterResource(id = R.drawable.puntitos_pantalla_conexion),
+                contentDescription = "Puntitos de conexión",
+                modifier = Modifier.size(40.dp) // Tamaño de los puntitos
+            )
+
+
+            Spacer(modifier = Modifier.height(20.dp))
+
+            // Icono de WiFi
+            Image(
+                painter = painterResource(id = R.drawable.wifi_si),
+                contentDescription = "WiFi",
+                modifier = Modifier.size(50.dp) // Tamaño del icono WiFi
+            )
+            Image(
+                painter = painterResource(id = R.drawable.puntitos_pantalla_conexion),
+                contentDescription = "Puntitos de conexión",
+                modifier = Modifier.size(30.dp) // Tamaño de los puntitos
+            )
+            Image(
+                painter = painterResource(id = R.drawable.puntitos_pantalla_conexion),
+                contentDescription = "Puntitos de conexión",
+                modifier = Modifier.size(20.dp) // Tamaño de los puntitos
+            )
+            Image(
+                painter = painterResource(id = R.drawable.puntitos_pantalla_conexion),
+                contentDescription = "Puntitos de conexión",
+                modifier = Modifier.size(10.dp) // Tamaño de los puntitos
+            )
+
+            Spacer(modifier = Modifier.height(30.dp))
+
+            // Icono de Móvil
+            Image(
+                painter = painterResource(id = R.drawable.movil),
+                contentDescription = "Móvil",
+                modifier = Modifier.size(80.dp) // Tamaño del icono del móvil
+            )
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun ConexionScreenPreview() {
+    ConexionScreen()
+}
---
+++ app/src/main/java/com/example/tvmaster/ConexionFailed.kt
@@ -0,0 +1,101 @@
+package com.example.tvmaster
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material.icons.filled.Settings
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+
+@Composable
+fun ConexionFallidaScreen() {
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(Color.Black)
+    ) {
+        // Botón de Volver (arriba a la izquierda)
+        Row (
+            modifier = Modifier
+                .align(Alignment.TopStart)
+                .padding(16.dp)
+        ){
+            IconButton(
+                onClick = { /* Acción de volver */ },
+                modifier = Modifier
+                    .padding(1.dp)
+            ) {
+                Icon(
+                    imageVector = Icons.Default.ArrowBack,
+                    contentDescription = "Volver",
+                    tint = Color.White
+                )
+            }
+            Text(
+                text = stringResource(id = R.string.failed_conection),
+                modifier = Modifier.padding(10.dp),
+                color = Color.White,
+                fontSize = 24.sp,
+                fontWeight = FontWeight.Bold
+
+            )
+        }
+
+
+        // Botón de Ajustes (arriba a la derecha)
+        IconButton(
+            onClick = { /* Acción de ajustes */ },
+            modifier = Modifier
+                .align(Alignment.TopEnd) // Lo alinea en la esquina superior derecha
+                .padding(16.dp)
+        ) {
+            Icon(
+                imageVector = Icons.Default.Settings,
+                contentDescription = "Ajustes",
+                tint = Color.White
+            )
+        }
+
+        // Contenido centrado (No conectado, ícono y texto de error)
+        Column(
+            modifier = Modifier
+                .align(Alignment.Center) // Centra todo en el medio de la pantalla
+                .padding(16.dp),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Center
+        ) {
+
+            Spacer(modifier = Modifier.height(20.dp))
+            Image(
+                painter = painterResource(id = R.drawable.wifi_no),
+                contentDescription = "Error de conexión",
+                modifier = Modifier.size(50.dp)
+            )
+            Spacer(modifier = Modifier.height(20.dp))
+            Text(
+                text = stringResource(id = R.string.large_failed_conection),
+                color = Color.White,
+                fontSize = 18.sp
+            )
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun PreviewConexionFallida() {
+    ConexionFallidaScreen()
+}
