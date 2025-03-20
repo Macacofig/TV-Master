42fbaa33dbac086618fb93d81b556d18a0bd0dcb
omkrfig <omkrfig@gmail.com>
2025-03-20 00:30:16 -0400

    Para control

---
+++ app/src/main/kotlin/com/control/app/ui/ControlScreen.kt
@@ -0,0 +1,9 @@
+package com.control.app.ui
+
+import androidx.compose.runtime.Composable
+import com.control.app.viewmodel.ControlViewModel
+
+@Composable
+fun ControlScreen(viewModel: ControlViewModel) {
+    // Aquí defines la UI con Jetpack Compose
+}
